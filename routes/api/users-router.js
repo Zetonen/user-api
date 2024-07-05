32dd11c53d3cc0e9575b4d7f5c026bf96830a93f
Seven <76436489+Zetonen@users.noreply.github.com>
2024-07-05 08:27:46 -0700

    add routing http request

--- routes/api/users-router.js
+++ routes/api/users-router.js
@@ -1,4 +1,9 @@
 import express from "express";
+import { isEmptyBody } from "../../middlewares/index.js";
+import { userSigninSchema, userSignupSchema } from "../../models/User.js";
+import usersController from "../../controllers/auth-controller.js";
+import authenticate from "../../middlewares/authenticate.js";
+import {validateBody} from "../../decorators/index.js";
 const usersRouter = express.Router();
 
 usersRouter.get("/", (req, res) => {
@@ -8,21 +13,21 @@ usersRouter.get("/", (req, res) => {
 });
 
 usersRouter.post(
-  "/register"
-  //   isEmptyBody,
-  //   validaterBody(userSignupSchema),
-  //   usersController.signup
+  "/signup",
+  isEmptyBody,
+  validateBody(userSignupSchema),
+  usersController.signup
 );
 
 usersRouter.post(
-  "/login"
-  //   isEmptyBody,
-  //   validaterBody(userSigninSchema),
-  //   usersController.signin
+  "/signin",
+  isEmptyBody,
+  validateBody(userSigninSchema),
+  usersController.signin
 );
 
-// usersRouter.post("/logout", authenticate, usersController.logout);
+usersRouter.post("/logout", authenticate, usersController.logout);
 
-// usersRouter.get("/current", authenticate, usersController.getCurrent);
+usersRouter.get("/current", authenticate, usersController.getCurrent);
 
 export default usersRouter;
